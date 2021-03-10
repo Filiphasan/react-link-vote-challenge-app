eaa7f2284ac92ba822cc9dd341fd13b2d58cbdcd
Hasan Erdal <58440290+Filiphasan@users.noreply.github.com>
2021-03-10 13:51:05 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container submit-container">
+      <div className="submit-box">
+        <div className="submit">
+          <a href="#" className="submit-link"><i className="fas fa-plus"></i></a>
+        </div>
+      </div>
     </div>
   );
 }
